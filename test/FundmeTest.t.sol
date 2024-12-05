48f9deeae287b5ad6537a92d4db4c99da5bfedfe
Shahid-Nawaz-Pahore <shahidnawaz.edu.pk@gmail.com>
2024-12-05 14:28:13 +0500

    ..

--- test/FundmeTest.t.sol
+++ test/FundmeTest.t.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.18;
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/fundme.sol";
+import {DeployFundMe} from "../script/fundme.s.sol";
 contract FundMeTest is Test{
     FundMe  fundme;
     function setUp() external {
-         fundme= new FundMe();
+       // fundme= new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+       DeployFundMe deployFundMe = new DeployFundMe();
+       fundme =  deployFundMe.run();
     }
 
     function  testMINIMUM_USD() public view {
@@ -16,7 +19,7 @@ contract FundMeTest is Test{
         console.log(fundme.i_owner());
         console.log(msg.sender);
         console.log(address(this));
-        assertEq(fundme.i_owner(), address(this));
+        assertEq(fundme.i_owner(), msg.sender);
     }
 
     function testgetVersion() public {
